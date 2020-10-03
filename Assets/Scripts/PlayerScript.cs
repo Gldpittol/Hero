aa684598ccd46af59f745c51235771a367707766
Gabriel Pittol <gabrielpittol@hotmail.com>
2020-10-02 23:24:38 -0300

    player movement and death script

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -4,15 +4,70 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private GameControllerScript gc;
+
+    private SpriteRenderer sr;
+    public Sprite upSprite;
+    public Sprite downSprite;
+    public Sprite rightSprite;
+    public Sprite leftSprite;
+
+    private bool canMove;
+    public float waitTime;
+
     void Start()
     {
-        
+        canMove = true;
+        gc = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameControllerScript>();
+        sr = GetComponent<SpriteRenderer>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if((Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W)) && canMove && transform.position.y < 0.63f)
+        {
+            canMove = false;
+            sr.sprite = upSprite;
+            gameObject.transform.position = new Vector2(transform.position.x, transform.position.y + 0.16f);
+            StartCoroutine(MoveDelay());
+        }
+
+        if ((Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S)) && canMove && transform.position.y > -0.79f)
+        {
+            canMove = false;
+            sr.sprite = downSprite;
+            gameObject.transform.position = new Vector2(transform.position.x, transform.position.y - 0.16f);
+            StartCoroutine(MoveDelay());
+        }
+
+        if ((Input.GetKeyDown(KeyCode.LeftArrow) || Input.GetKeyDown(KeyCode.A)) && canMove && transform.position.x > -0.70f)
+        {
+            canMove = false;
+            sr.sprite = leftSprite;
+            gameObject.transform.position = new Vector2(transform.position.x - 0.16f, transform.position.y);
+            StartCoroutine(MoveDelay());
+        }
+
+        if ((Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.D)) && canMove && transform.position.x < 0.71f)
+        {
+            canMove = false;
+            sr.sprite = rightSprite;
+            gameObject.transform.position = new Vector2(transform.position.x + 0.16f, transform.position.y);
+            StartCoroutine(MoveDelay());
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag("Death"))
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    public IEnumerator MoveDelay()
+    {
+        yield return new WaitForSeconds(waitTime);
+        canMove = true;
     }
 }
